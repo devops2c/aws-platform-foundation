a49df54dd2a1c75e115aeec51148ea366f2448e3
LLFQ6783 <mohamed.belhedi@sofrecom.com>
2026-03-01 08:25:50 +0100

    edit md

--- stacks/static-site/backend.tf
+++ stacks/static-site/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "terraform-state-mohamed-2025"
-    key     = "stacks/static-site/terraform.tfstate"
+    key     = "static-site/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
   }
